061cf6525e8e2f74289a936d604afb96b5a05d70
RyanHewitt <ryanjh2004@gmail.com>
2023-10-28 01:13:43 -0400

    Made Pause Menu

--- DapperSunsProject/Assets/Scripts/ButtonFunctions.cs
+++ DapperSunsProject/Assets/Scripts/ButtonFunctions.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonFunctions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Resume()
     {
-        
+        GameManager.instance.stateUnpause();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Restart()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.stateUnpause();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
     }
 }

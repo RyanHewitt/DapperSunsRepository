7801a06654c645935a18c8ba8fe93c19e489e59a
Kingdomempire <edking143@gmail.com>
2023-10-29 19:48:37 -0400

    EnemyAi base
    Needed to add AI navigation to the project. The enemy now moves and looks at what has the player tag. The enemy does not have the ability to shoot due to me seeing there is a gun and bullet script and I'm not sure how they work. could not test the take damage. added a basic material for flash when damage is taken. commented Shoot function and if !isShooting.

--- DapperSunsProject/Assets/Scripts/EnemyAi.cs
+++ DapperSunsProject/Assets/Scripts/EnemyAi.cs
@@ -3,26 +3,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemyAi : MonoBehaviour
+public class EnemyAi : MonoBehaviour, IDamage
 {
+    [Header("---Components---")]
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
+    [SerializeField] Color flash;
+    [SerializeField] Color returnColor;
+    [SerializeField] GameObject testLocation;
+    [SerializeField] Transform shootPos;
 
+    [Header("---Stats---")]
     [SerializeField] int HP;
     [SerializeField] int PlayerFaceSpeed;
 
+    //[Header("---Gun stats---")]
+    //[SerializeField] GameObject bullet;
+    //[SerializeField] int shootRate;
+
     Vector3 playerDirection;
-    bool isShooting;
+    //bool isShooting;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      //update goal here
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerDirection = GameManager.instance.player.transform.position - transform.position;
+
+        //if (!isShooting)
+        //{
+        //    StartCoroutine(Shoot());
+        //}
+
+        if (agent.remainingDistance < agent.stoppingDistance)
+        {
+            FaceTarget();
+        }
+        agent.SetDestination(GameManager.instance.player.transform.position);
+    }
+    
+    public void takeDamage(int Amount)
+    {
+        HP -= Amount;
+
+        StartCoroutine(FlashColor());
+
+        if(HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    //IEnumerator Shoot()
+    //{
+    //    isShooting = true;
+
+    //    Instantiate(bullet, shootPos.position, transform.rotation);
+
+    //    yield return new WaitForSeconds(shootRate);
+
+    //    isShooting = false;
+    //}
+    IEnumerator FlashColor()
+    {
+        model.material.color = flash;
+
+        yield return new WaitForSeconds(0.1f);
+
+        model.material.color = returnColor;
+    }
+    void FaceTarget()
+    {
+        Quaternion Rotation = Quaternion.LookRotation(playerDirection);
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Rotation, Time.deltaTime * PlayerFaceSpeed);
     }
 }

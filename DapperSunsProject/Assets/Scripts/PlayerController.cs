e0f4f236859d10fd9e06304dd127871b71ffa8ad
Sloz8 <sslozy10@gmail.com>
2023-10-30 18:31:10 -0400

    Cleaned Up
    Cleaned up some code and added headers in unity to make things easier to tinker with

--- DapperSunsProject/Assets/Scripts/PlayerController.cs
+++ DapperSunsProject/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("----- Components-----")]
     [SerializeField] CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
     [Range(1, 20)][SerializeField] float playerSpeed;
     [Range(1, 50)][SerializeField] float jumpHeight;
     [Range(-10, 50)][SerializeField] float gravityValue;
+
+    [Header("-----Player Stats-----")]
+    private Vector3 move;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
 
     private void Start()
     {
@@ -24,18 +28,13 @@ public class PlayerController : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
 
-        // Changes the height position of the player..
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            playerVelocity.y += jumpHeight;
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;

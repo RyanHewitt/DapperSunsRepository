abbf417d374254781f5851c5fd48e90fcaf46932
Sloz8 <sslozy10@gmail.com>
2023-10-30 17:59:13 -0400

    New configurations
    player speed, jump height, and gravity value are now all configurable

--- DapperSunsProject/Assets/Scripts/PlayerController.cs
+++ DapperSunsProject/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpHeight;
+    [SerializeField] float gravityValue;
 
     private void Start()
     {

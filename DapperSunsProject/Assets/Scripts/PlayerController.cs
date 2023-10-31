02cb5abda603fe315a08c80de56e63432503ddda
Sloz8 <sslozy10@gmail.com>
2023-10-31 14:47:30 -0400

    Player fully functions
    Player should be able to shoot, and take damage.  Added multiple jump options. Added player to prefabs.

--- DapperSunsProject/Assets/Scripts/PlayerController.cs
+++ DapperSunsProject/Assets/Scripts/PlayerController.cs
@@ -4,41 +4,101 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("----- Components-----")]
+    [Header("----- Components -----")]
     [SerializeField] CharacterController controller;
+
+    [Header("----- Player Stats -----")]
+    [Range(1, 10)][SerializeField] int HP;
     [Range(1, 20)][SerializeField] float playerSpeed;
     [Range(1, 50)][SerializeField] float jumpHeight;
+    [Range(1, 5)][SerializeField] int jumpMax;
     [Range(-10, 50)][SerializeField] float gravityValue;
 
-    [Header("-----Player Stats-----")]
+    [Header("----- Gun Stats -----")]
+    [SerializeField] int shootDamage;
+    [SerializeField] int shootDist;
+    [SerializeField] float shootRate;
+
     private Vector3 move;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
+    private int timesjumped;
+    bool isShooting;
+    int HPOriginal;
+
 
     private void Start()
     {
-
+        HPOriginal = HP;
     }
 
     void Update()
     {
+        if (Input.GetButton("Shoot") && !isShooting)
+        {
+            StartCoroutine(Shoot());
+        }
+
         groundedPlayer = controller.isGrounded;
+
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
+            timesjumped = 0;
         }
 
         move = Input.GetAxis("Horizontal") * transform.right +
                Input.GetAxis("Vertical") * transform.forward;
-        
-        controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        if (Input.GetButtonDown("Jump") && timesjumped < jumpMax)
         {
-            playerVelocity.y += jumpHeight;
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            timesjumped++;
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        controller.Move((move * Time.deltaTime * playerSpeed) + (playerVelocity * Time.deltaTime));
     }
+    IEnumerator Shoot()
+    {
+        isShooting = true;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDist))
+        {
+            IDamage damageable = hit.collider.GetComponent<IDamage>();
+
+            if (hit.transform != transform && damageable != null)
+            {
+                damageable.takeDamage(shootDamage);
+            }
+        }
+
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        //updatePlayerUI();
+        //StartCoroutine(GameManager.instance.playerFlashDamage());
+        //if (HP <= 0)
+        //{
+        //    GameManager.instance.youLose();
+        //}
+    }
+
+    //public void spawnPlayer()
+    //{
+    //    controller.enabled = false;
+    //    HP = HPOriginal;
+    //    transform.position = GameManager.instance.playerSpawnPos.transform.position;
+    //    controller.enabled = true;
+    //}
+
+    //public void updatePlayerUI()
+    //{
+    //    GameManager.instance.PlayerHPBar.fillAmount = (float)HP / HPOriginal;
+    //}
 }

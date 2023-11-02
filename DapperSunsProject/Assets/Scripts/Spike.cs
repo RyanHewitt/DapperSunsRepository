22ef449e54ad294c1816f3b9c429cb86d9b6b00e
Sloz8 <sslozy10@gmail.com>
2023-11-01 20:47:41 -0400

    Pulsing Spike
    Spike now pulses to the beat

--- DapperSunsProject/Assets/Scripts/Spike.cs
+++ DapperSunsProject/Assets/Scripts/Spike.cs
@@ -1,11 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Spike : MonoBehaviour
+public class Spike : Beat
 {
     [SerializeField] int SpikeDMG;
+    private Vector3 startsize;
+    [SerializeField] float pulse = 1.15f;
+    [SerializeField] float returnspeed = 5f;
 
+    protected override void Start()
+    {
+        base.Start();
+        startsize = transform.localScale;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        float newScaleY = Mathf.Lerp(transform.localScale.y, startsize.y, Time.deltaTime * returnspeed);
+        transform.localScale = new Vector3(startsize.x, newScaleY, startsize.z);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.isTrigger)
@@ -18,5 +34,10 @@ public class Spike : MonoBehaviour
         {
             damageable.takeDamage(SpikeDMG);
         }
+        
+    }
+    protected override void DoBeat()
+    {
+        transform.localScale = new Vector3(startsize.x, startsize.y * pulse, startsize.z);
     }
 }

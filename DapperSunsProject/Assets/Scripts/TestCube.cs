fd86f2d7e76470caaaf3ab9e2a74abc7ccc4ca82
RyanHewitt <ryanjh2004@gmail.com>
2023-11-06 21:40:19 -0500

    Testing navmesh with rigidbody on test cube

--- DapperSunsProject/Assets/Scripts/TestCube.cs
+++ DapperSunsProject/Assets/Scripts/TestCube.cs
@@ -2,13 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class TestCube : Beat, IDamage
+public class TestCube : Beat, IDamage, IBoop
 {
     [Range(1, 10)][SerializeField] int HP;
     bool isBig;
+    [SerializeField] NavMeshAgent agent;
+    Vector3 playerDirection;
+    [SerializeField] Rigidbody rb;
 
-   protected override void Start()
+    protected override void Start()
     {
         base.Start();
     }
@@ -16,20 +20,22 @@ public class TestCube : Beat, IDamage
     protected override void Update()
     {
         base.Update();
+        agent.SetDestination(GameManager.instance.player.transform.position);
+        playerDirection = GameManager.instance.player.transform.position - agent.transform.position;
     }
 
     protected override void DoBeat()
     {
-        if (isBig)
-        {
-            transform.localScale -= Vector3.one;
-            isBig = !isBig;
-        }
-        else
-        {
-            transform.localScale += Vector3.one;
-            isBig = !isBig;
-        }
+        //if (isBig)
+        //{
+        //    transform.localScale -= Vector3.one;
+        //    isBig = !isBig;
+        //}
+        //else
+        //{
+        //    transform.localScale += Vector3.one;
+        //    isBig = !isBig;
+        //}
     }
 
     public void takeDamage(int amount)
@@ -40,5 +46,10 @@ public class TestCube : Beat, IDamage
             Destroy(gameObject);
         }
 
+    }
+
+    public void DoBoop(float force)
+    {
+        rb.AddForce(-playerDirection * force, ForceMode.Impulse);
     }
 }

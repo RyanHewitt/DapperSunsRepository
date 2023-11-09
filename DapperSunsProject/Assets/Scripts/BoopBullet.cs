502cdb91bfed6f363b19333964b106c297f2f28a
RyanHewitt <ryanjh2004@gmail.com>
2023-11-08 20:49:48 -0500

    setup boop bullet to scale

--- DapperSunsProject/Assets/Scripts/BoopBullet.cs
+++ DapperSunsProject/Assets/Scripts/BoopBullet.cs
@@ -6,12 +6,17 @@ public class BoopBullet : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     [SerializeField] float bulletSpeed;
-    [SerializeField] float life = 3;
+    [SerializeField] float life;
 
-    public void Awake()
+    void Awake()
     {
         rb.velocity = -transform.forward * bulletSpeed;
         transform.localRotation *= Quaternion.Euler(90, 0, 0);
         Destroy(gameObject, life);
+    }
+
+    void Update()
+    {
+        //gameObject.transform.localScale *= 2f * Time.deltaTime;
     }
 }

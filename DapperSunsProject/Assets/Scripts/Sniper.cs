a393e6b2ca38749cf2b842bf015e5a8055946d00
Kingdomempire <edking143@gmail.com>
2023-11-11 14:54:42 -0500

    added fixed sniper. changed the sniper towers

--- DapperSunsProject/Assets/Scripts/Sniper.cs
+++ DapperSunsProject/Assets/Scripts/Sniper.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Sniper : Shooter
 {
     public LineRenderer laserLineRenderer;
-    public Transform _sniper;
-
-    protected void Update()
+    public Transform LazerPosition;
+    [SerializeField] int stepsOriginal = 3;
+    private int steps;
+    protected override void Start()
+    {
+        base.Start();
+        steps = stepsOriginal;
+    }
+    protected override void Update()
     {
         base.Update();
-        Vector3 aimDirection = _sniper.forward;
+
+        Vector3 aimDirection = LazerPosition.forward;
 
         Ray ray = new Ray(transform.position, aimDirection);
         RaycastHit hit;
@@ -28,6 +35,20 @@ public class Sniper : Shooter
         {
             // Disable the laser if it doesn't hit anything.
             laserLineRenderer.enabled = false;
+        }
+    }
+    protected override void Restart()
+    {
+        base.Restart();
+        steps = stepsOriginal;
+    }
+    protected override void BeatAction()
+    {
+        steps--;
+        if(steps <= 0)
+        {
+            base.BeatAction();
+            steps = stepsOriginal;
         }
     }
 }

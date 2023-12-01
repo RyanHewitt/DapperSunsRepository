3189d173b47e3f301125592bb5efad384eb659bd
ryan <120286082+diamondcougar10@users.noreply.github.com>
2023-12-01 12:45:28 -0500

    LevelLocked and Added to Button Array

--- DapperSunsProject/Assets/Scripts/LevelSelect.cs
+++ DapperSunsProject/Assets/Scripts/LevelSelect.cs
@@ -2,13 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
+    public Button[] buttons;
+    public GameObject levelButtons;
+
+    private void Awake()
+    {
+        ButtonsToArray();
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+        for(int i = 0;i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+
     public void OpenLevel(int levelid)
     {
         string levelname = "Level" + levelid;
         SceneManager.LoadScene(levelname);
     }
+
+    void ButtonsToArray()
+    {
+        int childCount = levelButtons.transform.childCount;
+        buttons = new Button[childCount];
+        for (int i = 0; i < childCount; i++)
+        {
+            buttons[i] = levelButtons.transform.GetChild(i).gameObject.GetComponent<Button>();
+        }
+    }
+
 
 }

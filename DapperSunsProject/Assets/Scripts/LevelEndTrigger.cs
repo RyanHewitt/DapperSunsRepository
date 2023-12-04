38e7a0e46f991fd202a274614713517c4eb57120
Sean Szalasny <sslozy10@gmail.com>
2023-12-03 20:06:14 -0500

    Medals now show up on win menu

--- DapperSunsProject/Assets/Scripts/LevelEndTrigger.cs
+++ DapperSunsProject/Assets/Scripts/LevelEndTrigger.cs
@@ -2,12 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelEndTrigger : MonoBehaviour
 {
-    int timer;
+    float timer;
+    [SerializeField] Image bronzebadge;
+    [SerializeField] Image silverbadge;
+    [SerializeField] Image goldbadge;
+    [SerializeField] Image diamondbadge;
+
     void OnTriggerEnter(Collider other)
     {
+        diamondbadge.enabled = false;
+        goldbadge.enabled = false;
+        silverbadge.enabled = false;
+        bronzebadge.enabled = false;
+
         if (other.isTrigger)
         {
             return;
@@ -16,7 +27,23 @@ public class LevelEndTrigger : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             GameManager.instance.PopupWin();
-            GameManager.instance.elapsedTime = timer;
+            timer = GameManager.instance.elapsedTime;
+            if(timer < 15.000000)
+            {
+                diamondbadge.enabled = true;
+            }
+            else if(timer < 30.000000)
+            {
+                goldbadge.enabled = true;
+            }
+            else if(timer < 45.000000)
+            {
+                silverbadge.enabled = true;
+            }
+            else if(timer > 60.000000)
+            {
+                bronzebadge.enabled = true;
+            }
         }
 
         UnlockNewLevel();

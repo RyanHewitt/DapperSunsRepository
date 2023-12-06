d0a04908a98a396f7733224c369006a2cff7c43d
Sloz8 <sslozy10@gmail.com>
2023-12-06 15:22:41 -0500

    ExitDoorUnpowered fully functional

--- DapperSunsProject/Assets/Scripts/DoorToggle.cs
+++ DapperSunsProject/Assets/Scripts/DoorToggle.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorToggle : MonoBehaviour
 {
     [SerializeField] GameObject Door;
+    [SerializeField] GameObject trigger;
+    [SerializeField] GameObject target;
     bool turnontoggle;
     private Vector3 startsize;
     [SerializeField] float pulse = 1.15f;
@@ -14,9 +17,9 @@ public class DoorToggle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Door.GetComponentInChildren<Collider>().enabled = false;
+        GameManager.instance.OnRestartEvent += Restart;
         turnontoggle = false;
-        Door.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        trigger.SetActive(false);
         Door.GetComponent<Renderer>().material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
         Door.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.green * 0);
 
@@ -28,15 +31,25 @@ public class DoorToggle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!target.GetComponent<Collider>().enabled)
+        {
+            turnontoggle = true;
+        }
         if(turnontoggle == true)
         {
+            trigger.SetActive(true);
             Door.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.green * EmissiveOn);
             transform.localScale = Vector3.Lerp(transform.localScale, startsize, Time.deltaTime * returnspeed);
-            Door.GetComponentInChildren<Collider>().enabled = true;
         }
     }
     void DoBeat()
     {
         transform.localScale = new Vector3(startsize.x, startsize.y * pulse, startsize.z);
+    }
+    void Restart()
+    {
+        turnontoggle = false;
+        trigger.SetActive(false);
+        Door.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.green * 0);
     }
 }
